6f23c580a462363b3d74a93c81193a0d217d0ea9
unknown <perrithomas@outlook.com>
2026-03-19 08:08:53 +0100

    added a sine lookuptable needed later for a dds

--- hardware/fra_zynq7015_pcie/fra_zynq7015_pcie.runs/impl_1/rundef.js
+++ hardware/fra_zynq7015_pcie/fra_zynq7015_pcie.runs/impl_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log fra_top.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source fra_top.tcl -notrace" );
 
